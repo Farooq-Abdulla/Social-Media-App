826e08b082ba20d02f077cee146755be0209a161
Farooq Abdulla Shaik <farooqabdulla279@gmail.com>
2024-10-16 22:37:59 -0500

    updated package.json

--- src/auth.ts
+++ src/auth.ts
@@ -4,6 +4,7 @@ import NextAuth from 'next-auth';
 import Google from 'next-auth/providers/google';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  trustHost:true,
   adapter: PrismaAdapter(prisma),
   callbacks: {
     session({ session, user }) {
